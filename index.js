351227765f6089c76024cfed5092b304ad28fe3c
i0dheerajdileep <i.dheerajdileep@gmail.com>
2024-02-25 06:30:44 +0530

    feat:npm package

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const Cadsy = ({ children }) => {
+const  Cadsy = ({ children }) => {
   
   const findVideoElement = () => {
     return document.getElementById('cadsyvideo');
@@ -9,7 +9,6 @@ const Cadsy = ({ children }) => {
   const captureAndPostFrames = async () => {
     // console.log(videoElement,"videoelement from capture and post frame")
     const canvas = document.createElement('canvas');
-    const ready = document.getElementById("cadsyvideo")
 
     const videoElement = document.querySelector('#cadsyvideo video');
     // Ensure the video element is present and ready
@@ -29,33 +28,58 @@ const Cadsy = ({ children }) => {
 
     try {
       // Perform two API requests concurrently
-      const [response1, response2] = await Promise.all([
+      const [response1, response2,response3] = await Promise.all([
         axios({
           method: "POST",
-          url: "https://detect.roboflow.com/boobsdetector/1",
+          url: "https://detect.roboflow.com/boobs-hlpjy/1",
           params: {
-            api_key: 'sZFjx8Fimj7ZtIWfDnwo'
+              api_key: "bDlLbarnCWb5q2UlZjnc"
+          },
+          data: image,
+          headers: {
+              "Content-Type": "application/x-www-form-urlencoded"
+          }
+      }),
+        axios({
+          method: "POST",
+          url: "https://detect.roboflow.com/dicks-b5fv4/1",
+          params: {
+              api_key: "YiMe3gOCmm3WgkvBImqY"
+          },
+          data: image,
+          headers: {
+              "Content-Type": "application/x-www-form-urlencoded"
+          }
+      }),
+        axios({
+          method: "POST",
+          url: "https://detect.roboflow.com/bikinis/1",
+          params: {
+            api_key: "UUD1qWjeQoBRsTOlhmhi"
           },
           data: image,
           headers: {
             "Content-Type": "application/x-www-form-urlencoded"
           }
-        }),
-        axios({
-          method: "POST",
-          url: "https://detect.roboflow.com/dickdetector/3",
-          params: {
-            api_key: '4WvQYKCiiFjRFkdUHdWw'
-          },
-          data: image,
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-          }
-        }),
+        })
       ]);
+        
 
       console.log('Image posted to API 1:', response1.data);
       console.log('Image posted to API 2:', response2.data);
+      console.log('Image posted to API 3:', response3.data);
+      if(response1.data.predictions.length > 0){
+        localStorage.setItem('vulgar', JSON.stringify(response1.data.predictions));
+        window.location.reload()
+      }
+      if(response2.data.predictions.length > 0){
+        localStorage.setItem('vulgar', JSON.stringify(response2.data.predictions));
+        window.location.reload()
+      }
+      if(response3.data.predictions.length > 0){
+        localStorage.setItem('vulgar', JSON.stringify(response3.data.predictions));
+        window.location.reload()
+      }
     } catch (error) {
       console.error('Error posting images to APIs:', error);
     }
@@ -72,6 +96,11 @@ const Cadsy = ({ children }) => {
 
   // Set interval to call processVideo every 5 seconds (5000 milliseconds)
   setInterval(processVideo, 5000);
+  const vulgarInLocalStorage = localStorage.getItem('vulgar');
+  if (vulgarInLocalStorage) {
+    alert("you are banned from this site");
+    return 
+  }
 
   return children;
 };
